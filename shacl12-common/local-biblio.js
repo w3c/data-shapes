81588b862f20ea63e9acfe88def6f61750a5770b
Nicholas Car <nick@kurrawong.ai>
2025-08-21 18:06:57 +1000

    local biblio test

---
+++ shacl12-common/local-biblio.js
@@ -0,0 +1,109 @@
+var localBibliography = {
+  ISO24707: {
+    id: "ISO24707",
+    title: "Information technology — Common Logic (CL) — A framework for a family of logic-based languages",
+    date: "2018-07",
+    href: "https://www.iso.org/standard/66249.html",
+    status: "Published",
+    publisher: "ISO/EIC"
+  },
+  HORST04: {
+    id: "HORST04",
+    title: "Extending the RDFS Entailment Lemma",
+    authors: ["Herman J. ter Horst"],
+    date: "2004",
+    isbn: "978-3-540-30475-3",
+    href: "https://doi.org/10.1007/978-3-540-30475-3_7",
+    status: "Conference Proceeding",
+    publisher: "Springer, Berlin, Heidelberg"
+  },
+  HORST05: {
+    id: "HORST05",
+    title: "Completeness, Decidability and Complexity of Entailment for RDF Schema and a Semantic Extension Involving the OWL Vocabulary",
+    authors: ["Herman J. ter Horst"],
+    date: "2005",
+    href: "https://dx.doi.org/10.2139/ssrn.3199251",
+    status: "Conference Proceeding",
+    publisher: "Journal of Web Semantics"
+  },
+  SAFETY: {
+    title: "Saftety of recursive horn clauses with infinite relations",
+    href: "http://arxiv.org/abs/cs.LO/9809032",
+    authors: [
+      "R. Ramakrishnan",
+      "F. Bancilhon",
+      "A. Silberschatz"
+    ],
+    date: "1987",
+    publisher: "ACM New York"
+  },
+  SAX: {
+    title: "SAX – The Simple API for XML",
+    href: "https://accu.org/journals/overload/7/34/cornish_515/",
+    authors: ["Steve Cornish"],
+    date: "October 1999",
+  },
+  STRIPEDRDF: {
+    id: "STRIPEDRDF",
+    title: "RDF: Understanding the Striped RDF/XML Syntax",
+    editors: ["D. Brickley"],
+    date: "2001",
+    href: "http://www.w3.org/2001/10/stripes/",
+    status: "Team Submission",
+    publisher: "W3C"
+  },
+  "RDF-STAR-CG": {
+    title: "RDF-star and SPARQL-star",
+    href: "https://w3c.github.io/rdf-star/cg-spec/2021-12-17.html",
+    authors: [
+      "Olaf Hartig",
+      "Pierre-Antoine Champin",
+      "Gregg Kellogg",
+      "Andy Seaborne"
+    ],
+    rawDate: "2021-12-17",
+    status: "Final Community Group Report",
+    publisher: "W3C"
+  },
+  "RDF12-NEW": {
+    "authors": [
+        "David Wood"
+    ],
+    "href": "https://w3c.github.io/rdf-new/spec/",
+    "title": "What’s New in RDF 1.2",
+    "rawDate": "2022-01-01",
+    "status": "DNOTE",
+    "publisher": "W3C"
+  },
+  "RDF12-PRIMER": {
+    "authors": [
+        "Guus Schreiber",
+        "Yves Raimond"
+    ],
+    "href": "https://w3c.github.io/rdf-primer/spec/",
+    "title": "RDF 1.2 Primer",
+    "rawDate": "2022-01-01",
+    "status": "DNOTE",
+    "publisher": "W3C"
+  },
+ "SPARQL12-NEW": {
+    "authors": [
+        "The W3C RDF &amp; SPARQL Working Group"
+    ],
+    "href": "https://w3c.github.io/sparql-new/spec/",
+    "title": "What’s New in SPARQL 1.2",
+    "rawDate": "2022-01-01",
+    "status": "WD",
+    "publisher": "W3C"
+  },
+  "SPARQL12-CONCEPTS": {
+    "authors": [
+        "The W3C RDF &amp; SPARQL Working Group"
+    ],
+    "href": "https://w3c.github.io/sparql-concepts/spec/",
+    "title": "SPARQL 1.2 Concepts",
+    "rawDate": "2022-01-01",
+    "status": "WD",
+    "publisher": "W3C"
+  },
+};
