a2c788787020d703c71aeecf2213a2d70d2d8c11
Nicholas Car <nick@kurrawong.ai>
2026-08-06 19:55:09 +1000

    improve example checking script #2

--- shacl12-profiling/examples/validate_examples.py
+++ shacl12-profiling/examples/validate_examples.py
@@ -1,11 +1,10 @@
 from pathlib import Path
-from kurra.file import merge
-from kurra.shacl import validate
 from rdflib import Graph
 
 prefixes = open("_prefixes.ttl").read()
 
 for f in sorted(Path(__file__).parent.glob("*.ttl")):
     if "_" not in f.name:
-        g = Graph().parse(data=prefixes + "\n\n" + open(f).read())
+        d = prefixes + "\n\n" + open(f).read()
+        g = Graph().parse(data=d)
         print(f"{f} OK")
